6ec5997a3b41c9fb0a34818a9c4b530cabc2e1e4
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-06-09 20:05:54 +0530

    Added Test Scripts for All the Sorting Algorithms

--- src/sortingAlgorithms/testScript.js
+++ src/sortingAlgorithms/testScript.js
@@ -54,3 +54,98 @@ export function bubbleSort(array) {
   }
   return array;
 }
+
+export function quickSort(array) {
+  quickSortHelper(array, 0, array.length - 1);
+  return array;
+}
+
+function quickSortHelper(array, startIdx, endIdx) {
+  if (startIdx >= endIdx) {
+    return;
+  }
+
+  const pivotIdx = startIdx;
+  let leftIdx = startIdx + 1;
+  let rightIdx = endIdx;
+  while (leftIdx <= rightIdx) {
+    if (array[leftIdx] > array[pivotIdx] && array[rightIdx] < array[pivotIdx]) {
+      const temp = array[leftIdx];
+      array[leftIdx] = array[rightIdx];
+      array[rightIdx] = temp;
+      leftIdx++;
+      rightIdx--;
+    }
+    if (array[leftIdx] <= array[pivotIdx]) {
+      leftIdx++;
+    }
+    if (array[pivotIdx] <= array[rightIdx]) {
+      rightIdx--;
+    }
+  }
+
+  const temp = array[pivotIdx];
+  array[pivotIdx] = array[rightIdx];
+  array[rightIdx] = temp;
+
+  const isLeftSubArraySmaller =
+    rightIdx - 1 - startIdx < endIdx - (rightIdx + 1);
+  if (isLeftSubArraySmaller) {
+    quickSortHelper(array, startIdx, rightIdx - 1);
+    quickSortHelper(array, rightIdx + 1, endIdx);
+  } else {
+    quickSortHelper(array, rightIdx + 1, endIdx);
+    quickSortHelper(array, startIdx, rightIdx - 1);
+  }
+}
+
+export function heapSort(array) {
+  buildMaxHeap(array);
+  for (let endIdx = array.length - 1; endIdx > 0; endIdx--) {
+    swap(0, endIdx, array);
+    heapifyDown(0, endIdx - 1, array);
+  }
+  return array;
+}
+
+function buildMaxHeap(array) {
+  const firstParentIdx = Math.floor((array.length - 2) / 2);
+  for (let i = firstParentIdx; i >= 0; i--) {
+    heapifyDown(i, array.length - 1, array);
+  }
+}
+
+function heapifyDown(parentIdx, endIdx, heap) {
+  let leftChildIdx = parentIdx * 2 + 1;
+  while (leftChildIdx <= endIdx) {
+    let largerChildIdx = leftChildIdx;
+    const rightChildIdx = parentIdx * 2 + 2 <= endIdx ? parentIdx * 2 + 2 : -1;
+    if (rightChildIdx !== -1 && heap[rightChildIdx] > heap[leftChildIdx]) {
+      largerChildIdx = rightChildIdx;
+    }
+    if (heap[parentIdx] > heap[largerChildIdx]) {
+      return;
+    } else {
+      swap(parentIdx, largerChildIdx, heap);
+      parentIdx = largerChildIdx;
+      leftChildIdx = parentIdx * 2 + 1;
+    }
+  }
+}
+
+function swap(i, j, array) {
+  const temp = array[j];
+  array[j] = array[i];
+  array[i] = temp;
+}
+
+export function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let j = i;
+    while (j > 0 && array[j] < array[j - 1]) {
+      swap(j, j - 1, array);
+      j--;
+    }
+  }
+  return array;
+}
