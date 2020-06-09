0c71d3d2d0b4fe4234230eaf7a83ed54fa930a5b
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-06-10 01:35:10 +0530

    Added Styling & Credits

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Credits from "./Credits/Credits";
 import SortingVisualizer from "./SortingVisualizer/SortingVisualizer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Credits />
       <SortingVisualizer />
     </div>
   );
