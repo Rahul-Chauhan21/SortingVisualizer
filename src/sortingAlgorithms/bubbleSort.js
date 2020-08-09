18ed4d38a3512186c0f99add6c5e3a762bdb92a5
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-08-10 00:55:29 +0530

    Changes Made to Animations Array

--- src/sortingAlgorithms/bubbleSort.js
+++ src/sortingAlgorithms/bubbleSort.js
@@ -5,18 +5,19 @@ export function getBubbleSortAnimations(array) {
   while (!isSorted) {
     isSorted = true;
     for (let i = 0; i < array.length - 1 - counter; i++) {
+      animations.push(["comparing", i, i + 1]);
       if (array[i] > array[i + 1]) {
-        animations.push([i, i + 1]);
-        animations.push([i, i + 1]);
-        animations.push([i, array[i + 1]]);
-        animations.push([i + 1, array[i]]);
-        const temp = array[i];
-        array[i] = array[i + 1];
-        array[i + 1] = temp;
+        animations.push(["swapping", i, i + 1]);
+        swap(array, i, i + 1);
         isSorted = false;
       }
     }
     counter++;
   }
-  return [animations, array];
+  return animations;
+}
+function swap(array, indexOne, indexTwo) {
+  const temp = array[indexOne];
+  array[indexOne] = array[indexTwo];
+  array[indexTwo] = temp;
 }
