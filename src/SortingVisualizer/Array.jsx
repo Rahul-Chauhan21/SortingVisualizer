b76e629a2ad3dc6a932b113cd84ec1167e59ea11
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-08-12 13:45:36 +0530

    Display Bar Number Conditions Added

--- src/SortingVisualizer/Array.jsx
+++ src/SortingVisualizer/Array.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Bar from "./Bar";
 import "./SortingVisualizer.css";
-const MAX_SIZE_DISPLAY_NUM = 30;
+let MAX_SIZE_DISPLAY_NUM = 30;
+if (window.innerWidth <= 420) {
+  MAX_SIZE_DISPLAY_NUM = 15;
+}
 class Array extends React.Component {
   render() {
     return (
